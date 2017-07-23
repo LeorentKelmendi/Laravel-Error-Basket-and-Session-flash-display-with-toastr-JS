a883712f2f84d6197218ac553f1bc96b0e515d7c
Leorent Kelmendi <leorent@hotmail.com>
2017-07-24 00:59:28 +0200

    VFinal
    Using toastr to catch both errors and session flash for Laravel. My
    default temp.

--- messages.blade.php
+++ messages.blade.php
@@ -1,22 +1,32 @@
-@if(count($errors) > 0)
-@foreach($errors->all() as $error)
-<div class="alert alert-danger">
-    {{$error}}
-</div>
+@if(count($errors)>0)
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" type="text/javascript"></script>
+ <script>
+    @foreach($errors->all() as $error)
+    
+                toastr.options = {
+                      "debug": false,
+                      "positionClass": "toast-bottom-right",
+                      "onclick": null,
+                      "fadeIn": 300,
+                      "fadeOut": 1000,
+                      "timeOut": 5000,
+                      "extendedTimeOut": 1000
+                    }
+                toastr.warning("{{$error}}")
 
-
-@endforeach
+    @endforeach
+</script>
 @endif
 
-
 @if(session('success'))
-
-
-   <script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" type="text/javascript"></script>
+ <script>
             @if(Session::has('success'))
                toastr.options = {
                       "debug": false,
-//                      "positionClass": "toast-bottom-right",
+                      "positionClass": "toast-bottom-right",
                       "onclick": null,
                       "fadeIn": 300,
                       "fadeOut": 1000,
@@ -25,10 +35,7 @@
                     }
                 toastr.success("{{Session::get('success')}}")
 
-        @endif
-
-
-
-        </script>
+ @endif
+</script>
 
 @endif
