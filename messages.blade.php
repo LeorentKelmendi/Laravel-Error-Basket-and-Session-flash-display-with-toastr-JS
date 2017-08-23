4c135d7865ab0fe0e2bacd1cdde1b5a7951c6ad6
Leorent Kelmendi <leorent@hotmail.com>
2017-08-23 20:34:47 +0200

    Update messages.blade.php

--- messages.blade.php
+++ messages.blade.php
@@ -1,41 +1,42 @@
-    @if(count($errors)>0)
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" type="text/javascript"></script>
-     <script>
-        @foreach($errors->all() as $error)
-        
-                    toastr.options = {
-                          "debug": false,
-                          "positionClass": "toast-bottom-right",
-                          "onclick": null,
-                          "fadeIn": 300,
-                          "fadeOut": 1000,
-                          "timeOut": 5000,
-                          "extendedTimeOut": 1000
-                        }
-                    toastr.error("{{$error}}")
+@if(count($errors)>0)
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" type="text/javascript"></script>
+ <script>
+    @foreach($errors->all() as $error)
     
-        @endforeach
-    </script>
-    @endif
-    
-    @if(session('success'))
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" type="text/javascript"></script>
-     <script>
-                @if(Session::has('success'))
-                   toastr.options = {
-                          "debug": false,
-                          "positionClass": "toast-bottom-right",
-                          "onclick": null,
-                          "fadeIn": 300,
-                          "fadeOut": 1000,
-                          "timeOut": 5000,
-                          "extendedTimeOut": 1000
-                        }
-                    toastr.success("{{Session::get('success')}}")
-    
-     @endif
-    </script>
-    
-    @endi
+                toastr.options = {
+                      "debug": false,
+                      "positionClass": "toast-bottom-right",
+                      "onclick": null,
+                      "fadeIn": 300,
+                      "fadeOut": 1000,
+                      "timeOut": 5000,
+                      "extendedTimeOut": 1000
+                    }
+                toastr.error("{{$error}}")
+
+    @endforeach
+</script>
+@endif
+
+@if(session('success'))
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" type="text/javascript"></script>
+ <script>
+            @if(Session::has('success'))
+               toastr.options = {
+                      "debug": false,
+                      "positionClass": "toast-bottom-right",
+                      "onclick": null,
+                      "fadeIn": 300,
+                      "fadeOut": 1000,
+                      "timeOut": 5000,
+                      "extendedTimeOut": 1000
+                    }
+                toastr.success("{{Session::get('success')}}")
+
+ @endif
+</script>
+
+@endif
+ 
